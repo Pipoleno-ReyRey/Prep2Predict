54fe60a0c33656fa4a68c66c7ab9f9ef29c37de9
reynaldo <reysuero2011@gmail.com>
2026-08-11 15:59:30 -0400

    generate csv_data_analysis dir | conn.atomic change

--- data_prepare/files_processor.py
+++ data_prepare/files_processor.py
@@ -1,9 +1,6 @@
-import numpy as np
 import pandas as pd
 from pandas.api.types import is_string_dtype, is_numeric_dtype
 from pathlib import Path
-import shutil
-import csv
 from fastapi import UploadFile, File
 from data_prepare import convert_int as ci
 import db_connection as db
@@ -11,29 +8,29 @@ from models.files_db_model import *
 
 async def save_file(file: UploadFile = File(...)):
     try:
-        path = Path("/mnt") / "d" / "csv_data_analysis" / str(file.filename)
-        with open(path, "wb") as f:
-            f.write(await file.read())
+        with db.conn.atomic():
+            Path("csv_data_analysis").mkdir(parents=True, exist_ok=True)
+            path = Path("csv_data_analysis", str(file.filename))
+            with open(path, "wb") as f:
+                f.write(await file.read())
 
-        df = pd.read_csv(path)
-        columns = df.columns.tolist()
-        nulls_columns = []
-        for column in columns:
-            if  df[column].isnull().any():
-                nulls_columns.append(column)
+            df = pd.read_csv(path)
+            columns = df.columns.tolist()
+            nulls_columns = []
+            for column in columns:
+                if df[column].isnull().any():
+                    nulls_columns.append(column)
 
-        db.conn.connect()
-        new_file = FilesDBModel()
-        new_file.path = str(path)
-        new_file.save()
-        db.conn.close()
+            new_file = FilesDBModel()
+            new_file.path = str(path)
+            new_file.save()
 
-        return {
-            "path": str(path),
-            "columns": columns,
-            "nulls_columns": nulls_columns,
-            "records": df.shape[0],
-        }
+            return {
+                "path": str(path),
+                "columns": columns,
+                "nulls_columns": nulls_columns,
+                "records": df.shape[0],
+            }
 
     except Exception as e:
         print(e)
